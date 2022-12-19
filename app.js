c7cf6ac331f88dfc02d9ec9dd4eb49864c500655
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-19 21:40:10 +0300

    add mobile-menu disappear on nav-link click

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('.menu-click');
 const bars = document.querySelector('.union');
 const close_btn = document.querySelector('.close-icon');
+const nav_links = document.querySelectorAll('.nav-link');
 
 bars.addEventListener('click', () => {
   menu.classList.add('open');
@@ -9,4 +10,10 @@ bars.addEventListener('click', () => {
 
 close_btn.addEventListener('click', () => {
   menu.classList.remove('open');
+});
+
+nav_links.forEach((nav_link) => {
+  nav_link.addEventListener('click', () => {
+    menu.classList.remove('open');
+  });
 });
