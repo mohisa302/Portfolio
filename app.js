e34f65ec7b30f3d67b0c171ca91917d5c397b3cd
mohi <mohisadeghi302@gmail.com>
2022-12-19 23:34:05 +0330

    Update js file

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 const menu = document.querySelector('.menu-click');
 const bars = document.querySelector('.union');
-const close_btn = document.querySelector('.close-icon');
-const nav_links = document.querySelectorAll('.nav-link');
+const closeBtn = document.querySelector('.close-icon');
+const navLinks = document.querySelectorAll('.nav-link');
 
 bars.addEventListener('click', () => {
   menu.classList.add('open');
-  console.log('click');
 });
 
-close_btn.addEventListener('click', () => {
+closeBtn.addEventListener('click', () => {
   menu.classList.remove('open');
 });
 
-nav_links.forEach((nav_link) => {
-  nav_link.addEventListener('click', () => {
+navLinks.forEach((navLink) => {
+  navLink.addEventListener('click', () => {
     menu.classList.remove('open');
   });
 });
