de720e525a60b437ad5ba0b9876d7cd51c4301db
mohi <mohisadeghi302@gmail.com>
2022-12-20 18:18:09 +0330

    add data file

---
+++ data.js
@@ -0,0 +1,75 @@
+const projectsList = [
+    {
+      id: 1,
+      name: 'Profesional Art Printing Data',
+      featuredImage: 'img/card2.jpg',
+      description: '',
+      button: 'see project',
+      technologies: [''],
+    },
+    {
+      id: 2,
+      name: 'Multi-post stories',
+      subtitle: ['canopy', 'back end dev', '2015'],
+      featuredImage: 'img/card3.jpg',
+      description:
+        'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+      button: 'see project',
+      technologies: ['html', 'css', 'javascript'],
+    },
+    {
+      id: 3,
+      name: 'Website Protfolio',
+      subtitle: ['canopy', 'back end dev', '2015'],
+      featuredImage: 'img/desktop/project-3.jpg',
+      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industrys standard',
+      button: 'see project',
+      technologies: ['html', 'css', 'javascript'],
+    },
+    {
+        id: 4,
+        name: 'Tonic',
+        subtitle: ['canopy', 'back end dev', '2015'],
+        featuredImage: 'img/desktop/project-4.jpg',
+        description:
+          'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+        button: 'see project',
+        technologies: ['html', 'css', 'javascript'],
+    },
+    {
+        id: 5,
+        name: 'Tonic',
+        subtitle: ['canopy', 'back end dev', '2015'],
+        featuredImage: 'img/desktop/project-4.jpg',
+        description:
+          'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+        button: 'see project',
+        technologies: ['html', 'css', 'javascript'],
+    },
+    {
+        id: 6,
+        name: 'Tonic',
+        subtitle: ['canopy', 'back end dev', '2015'],
+        featuredImage: 'img/desktop/project-4.jpg',
+        description:
+          'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+        button: 'see project',
+        technologies: ['html', 'css', 'javascript'],
+      },
+  ];
+  
+
+<div class="card card1">
+    <title class="cardt">${name}</title>
+<p class="cardp">
+  
+</p>
+<ul class="cardb">
+  <li class="bot button1">html</li>
+  <li class="bot button2">bootstrap</li>
+  <li class="bot button3">Ruby</li>
+</ul>
+<button class="link-button2" type="button">
+  <a class="ltext1" href="#">See Project</a>
+</button>
+</div>
