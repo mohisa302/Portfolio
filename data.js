3c1fdd84cafaf7faa007551af19ba6e9f49f0973
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-20 18:41:15 +0300

    add data and card info to data.js

--- data.js
+++ data.js
@@ -1,27 +1,32 @@
-const projectsList = [
-    {
-      id: 1,
-      name: 'Profesional Art Printing Data',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
-      featuredImage: 'img/card2.jpg',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
-      button: 'see project',
-      technologies: ['html', 'css', 'javascript'],
-    },
-    {
-      id: 2,
-      name: 'Multi-post stories',
-      featuredImage: 'img/card3.jpg',
-      description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
-      button: 'see project',
-      technologies: ['html', 'css', 'javascript'],
+const projectsData = [
+  {
+    id: 1,
+    name: 'Profesional Art Printing Data',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    featuredImage: 'img/card2.jpg',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    button: 'see project',
+    technologies: ['html', 'css', 'javascript'],
+  },
+  {
+    id: 2,
+    name: 'Multi-post stories',
+    featuredImage: 'img/card3.jpg',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    button: 'see project',
+    technologies: ['html', 'css', 'javascript'],
   },
   {
     id: 3,
     name: 'Profesional Art Printing Data',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     featuredImage: 'img/card4.jpg',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
   },
@@ -29,7 +34,8 @@ const projectsList = [
     id: 4,
     name: 'Multi-post stories',
     featuredImage: 'img/card4.jpg',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
   },
@@ -37,17 +43,55 @@ const projectsList = [
     id: 5,
     name: 'Multi-post stories',
     featuredImage: 'img/card3.jpg',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
   },
   {
-    id: 4,
+    id: 6,
     name: 'Multi-post stories',
     featuredImage: 'img/card4.jpg',
-    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
+    description:
+      'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry standard',
     button: 'see project',
     technologies: ['html', 'css', 'javascript'],
   },
-   
-  ];
+];
+
+const cardsContainer = document.querySelector('.cards-container');
+
+projectsData.forEach((project) => {
+  const {
+    id,
+    name,
+    featuredImage,
+    description,
+    button,
+    technologies,
+  } = project;
+
+  cardsContainer.innerHTML += `
+  <div class="card card${id}">
+    <title class="cardt">${name}</title>
+    <title class="cardt-des">Data</title>
+    <title class="cardt-des">Healthcare</title>
+    <p class="cardp-des">
+      ${description}
+    </p>
+    <p class="cardp">
+      A daily selection of privately personalized reads; no
+      accounts or sign-ups required. has been the industry's
+      standard
+    </p>
+    <ul class="cardb">
+      <li class="bot button1">${technologies[0]}</li>
+      <li class="bot button2">${technologies[1]}</li>
+      <li class="bot button3">${technologies[2]}</li>
+    </ul>
+    <button class="link-button2" type="button">
+      <a class="ltext1" href="#">${button}</a>
+    </button>
+  </div>
+  `;
+});
