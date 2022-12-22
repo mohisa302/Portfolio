bc7cd780d1c46d7ea457186a77f5d7b696d6a822
Martin Karugaba <martinkarugaba21@gmail.com>
2022-12-22 13:09:32 +0300

    validate form

--- js/form.js
+++ js/form.js
@@ -1,6 +1,12 @@
 const contactForm = document.querySelector('.form-action');
+const error = document.querySelector('small');
+const email = document.querySelector('#email');
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('form submitted');
+  if (email.value !== email.value.toLowerCase()) {
+    error.classList.add('error');
+  } else {
+    contactForm.submit();
+  }
 });
