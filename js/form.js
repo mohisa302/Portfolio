583cec2875966392eed312dfea9fd81217274c08
mohi <mohisadeghi302@gmail.com>
2022-12-22 21:23:02 +0330

    Save data in local data base

--- js/form.js
+++ js/form.js
@@ -1,6 +1,29 @@
 const contactForm = document.querySelector('.form-action');
 const error = document.querySelector('small');
-const email = document.querySelector('#email');
+const formInput = document.querySelectorAll('.form-data');
+const form = {
+  fullName: "1",
+  email: "2",
+  comment: "3"
+};
+
+formInput.forEach((input, index) => {
+  input.addEventListener('change', (event) => {
+    form.fullName = formInput[0].value;
+    form.email = formInput[1].value;
+    form.comment = formInput[2].value;
+    localStorage.setItem("form", JSON.stringify(form));
+    console.log(localStorage);
+  });
+});
+
+window.onbeforeunload = function() {
+  localStorage.setItem("form", JSON.stringify(form));
+}
+
+window.onload = function () {
+  form = JSON.parse(localStorage.getItem("form"));
+}
 
 contactForm.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -10,3 +33,5 @@ contactForm.addEventListener('submit', (e) => {
     contactForm.submit();
   }
 });
+
+
